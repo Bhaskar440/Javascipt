42cac3c5660b73d287c5ea6f46d1d768b15f8679
Bhaskar Shukla <166659705+Bhaskar440@users.noreply.github.com>
2025-10-06 07:45:38 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,13 @@
+let a = 0;
+console.log(typeof(a))
+//first a= 5, typeof is number, then a = "5",typeof is string, typeof a="5abc" is also string
+let b = Boolean(a);
+console.log(typeof(b));
+//after conversion typeof is number
+console.log(b);
+//after printing the b the output is NaN which is not a number 
+// null me same cheez return krri object with false and 0 me bhi smae 
+var f = 5
+var p = String(f)
+console.log(typeof p);
+//yaha type of me string hai value me 5 which is a number
