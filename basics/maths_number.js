892f237ab09dffd2afba15bcf6775cd5539f4c30
Bhaskar Shukla <166659705+Bhaskar440@users.noreply.github.com>
2025-10-06 20:17:36 +0000

    math_number

---
+++ basics/maths_number.js
@@ -0,0 +1,10 @@
+//stack memory for primitive datatype, heap for non primitive datatype 
+const score = 400.445
+console.log(score);
+
+const balance = new Number(10.23);
+console.log(balance)
+
+console.log(balance.toString.length)
+console.log(balance.toFixed(1))
+console.log(Math.floor(Math.random())*10+1)// math.random gives values between 0 and 1 
