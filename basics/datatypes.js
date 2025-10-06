ce9dd240851da3dba7065623433f71546aa20b56
Bhaskar Shukla <166659705+Bhaskar440@users.noreply.github.com>
2025-10-06 06:27:50 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,6 @@
+let a = 1000000000000000000000000000000000000000000000000
+console.log(typeof(a));
+let b = null;
+console.log(typeof(b));//null is type of object
+let c = undefined;
+console.log(typeof(c));//undefined is type of undefined
